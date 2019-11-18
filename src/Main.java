4cf2813dfa888c023f2fbc738412a5ea0bf27bc1
inna-s <i.selezko@gmail.com>
2019-11-18 11:09:03 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        boolean isTrue = true;
+        if (isTrue) {
+            int i = 0;
+            i += 10;
+            System.out.println(i);
+        } else {
+            int i = 0;
+            i -= 10;
+            System.out.println(i);
+        }
+    } }
+
